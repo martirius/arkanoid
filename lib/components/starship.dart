31d6544c8c79bba9b15e9f026f6bad4362c46195
Mattia Pini <mattopigna_95@hotmail.com>
2022-05-14 21:48:19 +0200

    Add new components Add Brick component with correct animations and type Add PowerUp component with correct animations and type (effects TODO) Add BaseLevel with reference to all game components Add Level1 (missing correct powerups)

--- lib/components/starship.dart
+++ lib/components/starship.dart
@@ -1,8 +1,10 @@
 import 'package:arkanoid/components/field.dart';
+import 'package:arkanoid/components/inputs/button_interactable.dart';
 import 'package:arkanoid/main.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
+import 'package:flame_audio/flame_audio.dart';
 
 enum StartshipState {
   still,
@@ -12,8 +14,11 @@ enum StartshipState {
   collidingRight
 }
 
-class Starship extends SpriteComponent
-    with CollisionCallbacks, HasGameRef<Arkanoid> {
+enum StarshipAnimation { normal, appearing }
+
+class Starship extends SpriteAnimationGroupComponent<StarshipAnimation>
+    with CollisionCallbacks, HasGameRef<Arkanoid>
+    implements ButtonInteractable {
   final JoystickComponent _joystickComponent;
   Starship(this._joystickComponent)
       : super(
@@ -26,27 +31,48 @@ class Starship extends SpriteComponent
 
   StartshipState _state = StartshipState.still;
 
-  static const double starshipSpeed = 2.5;
+  static const double starshipSpeed = 3.5;
 
   static const double startshipWidth = 32;
   static const double startshipHeight = 8;
 
+  late final spritesNormal = [0, 1, 2, 3, 4, 5].map((i) => Sprite.load(
+        'starship.png',
+        srcPosition: Vector2(32.0, 8.0 * i),
+        srcSize: Vector2(startshipWidth, startshipHeight),
+      ));
+  late final spritesAppearing = [0, 1, 2, 3, 4].map((i) => Sprite.load(
+        'starship.png',
+        srcPosition: Vector2(0.0, 8.0 * i),
+        srcSize: Vector2(startshipWidth, startshipHeight),
+      ));
   @override
   Future<void>? onLoad() async {
     await Flame.images.load('starship.png');
-    sprite = await Sprite.load(
-      'starship.png',
-      srcPosition: Vector2(32, 39),
-      srcSize: Vector2(startshipWidth, startshipHeight),
-    );
-    position =
-        Vector2((gameRef.size.x / 2) - size.x / 2, (gameRef.size.y / 2) + 35);
+    await FlameAudio.audioCache.load('Game_Start.ogg');
+    animations = {
+      StarshipAnimation.normal: SpriteAnimation.spriteList(
+          await Future.wait(spritesNormal),
+          stepTime: 0.2),
+      StarshipAnimation.appearing: SpriteAnimation.spriteList(
+          await Future.wait(spritesAppearing),
+          stepTime: 0.2,
+          loop: false),
+    };
+    current = StarshipAnimation.appearing;
     add(RectangleHitbox());
+    FlameAudio.play('Game_Start.ogg');
   }
 
   @override
   void update(double dt) {
     super.update(dt);
+    //animation controller
+    if (current == StarshipAnimation.appearing &&
+        animation!.isLastFrame &&
+        animation!.elapsed >= 1) {
+      current = StarshipAnimation.normal;
+    }
 
     if (_joystickComponent.direction == JoystickDirection.left &&
         _state != StartshipState.collidingLeft) {
@@ -69,10 +95,6 @@ class Starship extends SpriteComponent
     }
   }
 
-  void firePressed() {
-    print("fire!");
-  }
-
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollision(intersectionPoints, other);
@@ -85,5 +107,11 @@ class Starship extends SpriteComponent
         x -= starshipSpeed;
       }
     }
+  }
+
+  @override
+  void onButtonPressed() {
+    print("fire!");
+    //TODO: add powerup effect here
   }
 }
